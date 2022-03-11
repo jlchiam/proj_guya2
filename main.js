01087efcd3d4ecb744d00a6b528eeb67d87644e6
Jolene Chiam <jolene_chiam@rp.edu.sg>
2022-03-11 11:32:54 +0800

    added database connection pool in main.js

--- main.js
+++ main.js
@@ -3,12 +3,50 @@ const cors = require("cors");
 const { router } = require("./routers");
 const app = express();
 
+var http = require('http').Server(app);
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+app.get('/api/database/status',function(req,res) {
+	console.log('API CALL: /api/database/status');
+	var retvalSettingValue = "?";
+	mysql_pool.getConnection(function(err, connection) {
+		if (err) {
+			connection.release();
+	  		console.log(' Error getting mysql_pool connection: ' + err);
+	  		throw err;
+	  	}
+	    connection.query('SELECT SettingValue FROM your_database_table WHERE SettingKey =\'DatabaseStatus\'', function(err2, rows, fields) {	
+	    	if (err2) {
+				var data = { "Time":"", "DatabaseStatus":"" };
+				data["Time"] = (new Date()).getTime();
+				data["DatabaseStatus"] = "Down";
+				res.json(data); 
+			} else {
+				var dbretval = rows[0].SettingValue;
+				if (dbretval == 1 ) {
+					var data = { "Time":"", "DatabaseStatus":"" };
+					data["Time"] = (new Date()).getTime();
+					data["DatabaseStatus"] = "Up";
+					res.json(data); 
+				} else {
+					var data = { "Time":"", "DatabaseStatus":"" };
+					data["Time"] = (new Date()).getTime();
+					data["DatabaseStatus"] = "Down";
+					res.json(data); 
+				}
+			}
+			console.log(' mysql_pool.release()');
+			connection.release();
+	    });
+	});
+});
+
 
 app.use(router);
 
