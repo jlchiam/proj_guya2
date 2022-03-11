497367d5a8f1a06ef34020364a4a49ebc47e0fcb
Jolene Chiam <jolene_chiam@rp.edu.sg>
2022-03-11 11:39:14 +0800

    put in db connection

--- database.js
+++ database.js
@@ -1,12 +1,27 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
+/*
+DBHOST=137.132.92.94
+DBUSER=fintechlab
+DBPASSWD=FinTechLab
+DBPORT=12865
+DBNAME=b16_group3
+
 let connection = mysql.createConnection({
   host: process.env.DBHOST,
   port: process.env.DBPORT,
   user: process.env.DBUSER,
   password: process.env.DBPASSWD,
   database: process.env.DBNAME,
+});
+*/
+let connection = mysql.createConnection({
+  host: "137.132.92.94",
+  port: "12865",
+  user: "fintechlab",
+  password: "FinTechLab",
+  database: "b16_group3"
 });
 
 connection.connect((error) => {
